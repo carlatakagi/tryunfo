2652b12c8fa7e2a03bbd7d422afb056d25ba9c4d
carlatakagi <carlatakagi@gmail.com>
2022-01-28 16:48:47 -0400

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+/*   constructor() {
+    super();
+
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({
+      [name]: value,
+    });
+  } */
+
   render() {
+    /* const { cardName, cardDescription } = this.props; */
     return (
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
       </div>
     );
   }
